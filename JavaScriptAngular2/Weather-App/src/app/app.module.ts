5e226f1557ee4ca62ac6c8d90de16c31afa1ebc1
Refayat Haque <refayathaque@gmail.com>
2017-08-14 17:26:20 -0400

    Working on Angular2 Weather App

--- JavaScriptAngular2/Weather-App/src/app/app.module.ts
+++ JavaScriptAngular2/Weather-App/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms'; // <-- Import FormsModule
+import { HttpModule } from '@angular/http'; // <-- Import HttpModule
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, // <-- Include module in our AppModules
+    HttpModule // <-- Include module in our AppModules
   ],
   providers: [],
   bootstrap: [AppComponent]
