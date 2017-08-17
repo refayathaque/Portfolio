0bf68b8257d5ccc0a49fe3f63135b5ba31f6a2f1
Refayat Haque <refayathaque@gmail.com>
2017-08-17 19:38:05 -0400

    Progress

---
+++ DailyAlgorithmsPractice.js
@@ -0,0 +1,22 @@
+// FizzBuzz
+function fizzbuzz(){
+    for(i = 1; i <= 100; i++) {
+        if(i % 3 === 0 && i % 5 === 0) {
+            console.log('FizzBuzz');
+            continue;
+        }
+        if(i % 3 === 0) {
+            console.log('Fizz');
+            continue;
+        }
+        if(i % 5 === 0) {
+            console.log('Buzz');
+            continue;
+        }
+        else {
+            console.log(i);
+        }
+    }
+}
+
+fizzbuzz();
