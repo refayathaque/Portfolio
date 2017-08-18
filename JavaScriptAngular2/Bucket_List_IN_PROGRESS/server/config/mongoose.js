f499827ff4ef5de4f14d96ce4b600000c02e613d
Refayat Haque <refayathaque@gmail.com>
2017-08-18 11:32:11 -0400

    Progress

---
+++ JavaScriptAngular2/Bucket_List_IN_PROGRESS/server/config/mongoose.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+var fs = require('fs');
+
+var path = require('path');
+
+var models_path = path.join(__dirname, './../models');
+
+// CHANGE DB NAME!
+mongoose.connect('mongodb://localhost/beltAttempt1');
+
+fs.readdirSync(models_path).forEach(function(file) {
+  if(file.indexOf('.js') >= 0) {
+    require(models_path + '/' + file);
+  }
+});
---
+++ JavaScriptAngular2/Bucket_List_IN_PROGRESS/server/config/routes.js
@@ -0,0 +1,47 @@
+// var users = require('../controllers/users.js')
+//
+// var questions = require('../controllers/questions.js')
+//
+// var answers = require('../controllers/answers.js')
+// IMPORTANT TO HAVE ALL CONTROLLERS HERE!
+
+module.exports = function(app) {
+
+    // app.post('/users', (req, res) => {
+    //     console.log('REQ BODY : ', req.body)
+    //     users.create(req, res)
+    // })
+    //
+    // app.post('/login', (req,res) => {
+    //     console.log('REQ BODY : ', req.body)
+    //     users.login(req, res)
+    // })
+    //
+    // app.post('/newquestions', (req, res) => {
+    //     console.log('REQ BODY : ', req.body)
+    //     questions.create(req, res)
+    // })
+    //
+    // app.post('/newanswers/:id', (req, res) => {
+    //     console.log('REQ BODY : ', req.body)
+    //     answers.create(req, res)
+    // })
+    //
+    // app.get('/index', (req,res) => {
+    //     console.log("Inside Routes (Home)")
+    //     console.log('REQ BODY : ', req.body)
+    //     questions.showAllQuestions(req, res)
+    // })
+    //
+    // app.post('/answers/:id/like', (req,res)=>{
+    //     console.log("inside routes")
+    //     answers.like(req, res);
+    // })
+    //
+    // app.get('/questions/:id', (req,res)=>{
+    //     console.log("Inside routes, getting answers sorted by likes for question with ID passed in")
+    //     console.log(req.body)
+    //     questions.show(req, res)
+    // })
+
+}
