3be51c98cb9fdb13cf987c7dbe217e4a4c13739b
Refayat Haque <refayathaque@gmail.com>
2017-08-20 12:17:20 -0400

    Progress

--- JavaScriptAngular2/Bucket_List_IN_PROGRESS/public/src/app/app.module.ts
+++ JavaScriptAngular2/Bucket_List_IN_PROGRESS/public/src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-
-import { AppComponent } from './app.component';
-
 import { FormsModule } from '@angular/forms';
-
 import { HttpModule } from '@angular/http';
 
 import { BucketListService } from "app/bucket-list.service";
+
+import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
