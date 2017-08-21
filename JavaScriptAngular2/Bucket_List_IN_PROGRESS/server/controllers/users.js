9b575a4e1646474f77375cb8d182b1dafdc095f7
Refayat Haque <refayathaque@gmail.com>
2017-08-21 10:04:02 -0400

    Yay!

--- JavaScriptAngular2/Bucket_List_IN_PROGRESS/server/controllers/users.js
+++ JavaScriptAngular2/Bucket_List_IN_PROGRESS/server/controllers/users.js
@@ -5,7 +5,12 @@ var User = mongoose.model('User');
 function UsersController() {
 
     this.create = function(req, res) {
-        // User.findOne({email : req.body.email})
+
+        // User.findOne({email : req.body.email}, function(err, user){
+        //
+        // })
+
+        console.log('Data being sent from Routes to Controller : ', req.body);
 
         User.findOne({
             $or: [
@@ -14,13 +19,15 @@ function UsersController() {
             ]
         })
         .then((user) => {
-            if(user.email === req.body.email) {
-                console.log('EMAIL', user.email);
-                res.json({error : true, messages : 'You are already registered'})
-            }
-            if(user.username === req.body.username) {
-                console.log('USERNAME', user.username);
-                res.json({error : true, messages : 'Username already in use'})
+            if(user) {
+                if(user.email === req.body.email) {
+                    console.log('EMAIL', user.email);
+                    res.json({error : true, messages : 'You are already registered'})
+                }
+                if(user.username === req.body.username) {
+                    console.log('USERNAME', user.username);
+                    res.json({error : true, messages : 'Username already in use'})
+                }
             }
             else {
                 var user = new User(req.body)
@@ -35,7 +42,7 @@ function UsersController() {
             }
         })
         .catch((err) => {
-            console.log('Error in users.js controller')
+            console.log('Error in users.js controller', err)
         })
     }
 
