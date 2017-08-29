939fdd926c77c0965892aeca892b2c093f6bb27a
Refayat Haque <refayathaque@gmail.com>
2017-08-28 21:37:34 -0400

    Finished Daily Reverse Array Practice

--- AlgorithmPractice/DailyReverseArray.js
+++ AlgorithmPractice/DailyReverseArray.js
@@ -12,7 +12,7 @@ reverseArray([1, 2, 3, 4, 5]);
 console.log('---------------------')
 
 //Another way to do it
-function reverseArray2(arr) {
+function reverseArrayALT(arr) {
   var temp = 0;
   for(var x = 0; x < (arr.length / 2); x++) {
     temp = arr[(arr.length - 1) - x];
@@ -21,5 +21,20 @@ function reverseArray2(arr) {
   }
   console.log(arr); //Outputs [5, 4, 3, 2, 1]
 }
-reverseArray2([1, 2, 3, 4, 5]);
+reverseArrayALT([1, 2, 3, 4, 5]);
 console.log('---------------------')
+
+////////////////////////////////////
+
+function reverseArray1(array) {
+    var temp = 0;
+    for (l = 0, r = (array.length - 1); l < r; l++, r--) {
+        temp = array[r];
+        array[r] = array[l];
+        array[l] = temp;
+    }
+    return(array)
+}
+console.log(reverseArray1([9, 7, 5, 3])); //Outputs [3, 5, 7, 9]
+// 'l < r' ensures that this works for odd length arrays as well,
+// basically says that 'l !== r', so the indexes can't be same
