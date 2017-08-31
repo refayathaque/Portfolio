790dffac846a4b1934618b45ac80d82fc9b3f8fb
Refayat Haque <refayathaque@gmail.com>
2017-08-31 11:20:00 +0400

    refactored add item function in items controller, but issues when trying to add items with a friend

--- JavaScriptAngular2/Bucket_List_IN_PROGRESS/server/controllers/items.js
+++ JavaScriptAngular2/Bucket_List_IN_PROGRESS/server/controllers/items.js
@@ -86,7 +86,7 @@ function ItemsController() {
     }
 
     this.addItem = function(req, res) {
-        console.log('(ITEMS CONTROLLER) REQ.BODY : ', req.body);
+        console.log('(ITEMS CONTROLLER) [ADD ITEM] ITEM REQ.BODY : ', req.body);
         Item.findOne({
             $or: [
                 {title : req.body.title},
@@ -94,73 +94,53 @@ function ItemsController() {
             ]
         })
         .then((data) => {
-            if(data) { // We need to FIRST check if 'data' returned anything from the query above or is null, only after checking to see if 'data' has a value can we go into it to do our title and description checks, if it's null the else statement gets run and we store the req.body data to our db
+            if(data) { // We need to FIRST check if 'data' returned anything from the query above or is null, only after checking to see if 'data' has a value can we go into it to do our title and description checks, if it's null the else statement gets run and we store the req.body into our db
                 if(data.title === req.body.title) {
-                    console.log('(ITEMS CONTROLLER) TITLE ALREADY IN DB : ', data.title);
+                    console.log('(ITEMS CONTROLLER) [ADD ITEM] TITLE ALREADY IN DB : ', data.title);
                     res.json({error : true, messages : 'Item previously added'})
                 }
                 if(data.description === req.body.description) {
-                    console.log('(ITEMS CONTROLLER) DESCRIPTION ALREADY IN DB : ', data.description);
+                    console.log('(ITEMS CONTROLLER) [ADD ITEM] DESCRIPTION ALREADY IN DB : ', data.description);
                     res.json({error : true, messages : 'Description previously added'})
                 }
             } //if(data)
             else {
-                User.findOne({_id: req.body.creator}, function(err, user) {
-                    var item = new Item(req.body);
-                    // item._creator = user._id;
-                    console.log('(ITEMS CONTROLLER) USER DATA : ', user);
-                    if(req.body.friend === req.body.creator) {
-                        res.json({error : true, messages : 'You cannot add yourself as a friend'})
+                // User.findOne({_id: req.body.creator}, function(err, user) {
+                var item = new Item(req.body);
+                // console.log('(ITEMS CONTROLLER) USER DATA : ', user);
+                item.save((err, item) => {
+                    if (!req.body.friend) {
+                        User.findOneAndUpdate(
+                            { _id : req.body.creator },
+                            { $push : { items : item } }, // When pushing into arrays we don't need to $set, $set is only for updating specific fields with specific data, not for pushing into arrays
+                            { returnNewDocument : true },
+                            function(err, item) {
+                                if(err) {
+                                    console.log('(ITEMS CONTROLLER) ERROR : ', err)
+                                    res.json({error : true, messages : 'Error with inserting data into db'})
+                                } else {
+                                    // console.log('(ITEMS CONTROLLER) USER DATA POST UPDATE WITH ITEM : ', user);
+                                    res.json({error : false, item : item})
+                                }
+                            } //function(err, item)
+                        ) //findOneAndUpdate
                     }
-                    else {
-                        item.save((err, item) => {
-                            if(req.body.friend && req.body.friend !== req.body.creator) {
-                                User.findOneAndUpdate(
-                                    { _id : req.body.creator },
-                                    { $push : { items : item } }, // When pushing into arrays we don't need to $set, $set is only for updating specific fields with specific data, not for pushing into arrays
-                                    { returnNewDocument : true },
-                                    function(err, item) {
-                                        if(err) {
-                                            console.log('(ITEMS CONTROLLER) ERROR : ', err)
-                                            res.json({error : true, messages : 'Error with inserting data into db'})
-                                        } else {
-                                            console.log('(ITEMS CONTROLLER) USER DATA POST UPDATE WITH ITEM : ', user);
-                                            res.json({error : false, item : item})
-                                        }
-                                    } //function(err, item)
-                                ) //findOneAndUpdate
-                                User.updateMany(
-                                    { _id : req.body.creator, _id : req.body.friend }, { $push : { items: item } },
-                                    function(err, item) {
-                                        if(err) {
-                                            console.log('(ITEMS CONTROLLER) ERROR : ', err)
-                                            res.json({error : true, messages : 'Error with inserting data into db'})
-                                        } else {
-                                            console.log('(ITEMS CONTROLLER) USER DATA POST UPDATE WITH ITEM : ', user);
-                                            res.json({error : false, item : item})
-                                        }
-                                    } //function(err, item)
-                                )
-                            }
-                            else {
-                                User.findOneAndUpdate(
-                                    { _id : req.body.creator },
-                                    { $push : { items : item } }, // When pushing into arrays we don't need to $set, $set is only for updating specific fields with specific data, not for pushing into arrays
-                                    { returnNewDocument : true },
-                                    function(err, item) {
-                                        if(err) {
-                                            console.log('(ITEMS CONTROLLER) ERROR : ', err)
-                                            res.json({error : true, messages : 'Error with inserting data into db'})
-                                        } else {
-                                            console.log('(ITEMS CONTROLLER) USER DATA POST UPDATE WITH ITEM : ', user);
-                                            res.json({error : false, item : item})
-                                        }
-                                    } //function(err, item)
-                                ) //findOneAndUpdate
-                            }
-                        }) //item.save
+                    if (req.body.friend) {
+                        User.updateMany(
+                            { _id : req.body.creator, _id : req.body.friend }, { $push : { items: item } },
+                            function(err, item) {
+                                if(err) {
+                                    console.log('(ITEMS CONTROLLER) ERROR : ', err)
+                                    res.json({error : true, messages : 'Error with inserting data into db'})
+                                } else {
+                                    // console.log('(ITEMS CONTROLLER) USER DATA POST UPDATE WITH ITEM : ', user);
+                                    res.json({error : false, item : item})
+                                }
+                            } //function(err, item)
+                        )
                     }
-                }) //findOne
+                }) //item.save
+                // }) //findOne
             } //else
         }) //then
         .catch((err) => {
