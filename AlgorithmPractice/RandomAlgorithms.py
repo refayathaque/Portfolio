8215464aafb2af40c7838a14253f44728567a549
Refayat Haque <refayathaque@gmail.com>
2017-09-25 11:54:10 -0400

    hello

--- AlgorithmPractice/RandomAlgorithms.py
+++ AlgorithmPractice/RandomAlgorithms.py
@@ -51,3 +51,32 @@ def reversearrayoneALT(list):
 w = [1, 2, 3, 4, 5]
 print reversearrayoneALT(w)
 print('------------09/21/17------------')
+
+def fizzbuzz3():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print 'FizzBuzz'
+        elif i % 3 == 0:
+            print 'Fizz'
+        elif i % 5 == 0:
+            print 'Buzz'
+        else:
+            print i
+        i += 1
+fizzbuzz3()
+print('------------09/25/17------------')
+
+def fizzbuzz4():
+    i = 1
+    while i < 101:
+        if i % 3 == 0 and i % 5 == 0:
+            print 'FizzBuzz'
+        elif i % 3 == 0:
+            print 'Fizz'
+        elif i % 5 == 0:
+            print 'Buzz'
+        else:
+            print i
+        i += 1
+fizzbuzz4()
+print('------------09/25/17------------')
