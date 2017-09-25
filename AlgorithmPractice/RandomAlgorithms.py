2a29fa3d774fbbdca5eaf0b37d0f76b52662e173
Refayat Haque <refayathaque@gmail.com>
2017-09-25 12:53:15 -0400

    did python algz

--- AlgorithmPractice/RandomAlgorithms.py
+++ AlgorithmPractice/RandomAlgorithms.py
@@ -80,3 +80,29 @@ def fizzbuzz4():
         i += 1
 fizzbuzz4()
 print('------------09/25/17------------')
+
+def reverselisttwo(list):
+    temp = 0
+    for i in range (0, len(list) / 2):
+        temp = list[i]
+        list[i] = list[(len(list) - 1) - i]
+        list[(len(list) - 1) - i] = temp
+    return list
+e = [1, 2, 3, 4, 5, 6, 7, 8]
+print reverselisttwo(e)
+print('------------09/25/17------------')
+
+def reverselisttwoALT(list):
+    temp = 0
+    left = 0
+    right = len(list) - 1
+    while left < right:
+        temp = list[left]
+        list[left] = list[right]
+        list[right] = temp
+        left += 1
+        right -= 1
+    return list
+r = [1, 2, 3, 4, 5, 6, 7, 8]
+print reverselisttwoALT(r)
+print('------------09/25/17------------')
