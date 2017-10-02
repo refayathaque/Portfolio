94887e52a5f34579ed6427b192d043e61ab4116c
Refayat Haque <refayathaque@gmail.com>
2017-10-02 19:07:04 -0400

    daily reverse array practice

--- AlgorithmPractice/DailyReverseArray.js
+++ AlgorithmPractice/DailyReverseArray.js
@@ -415,3 +415,33 @@ function reverseArray27(array) {
 qw = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 console.log(reverseArray27(qw));
 console.log('---------09/28/17------------');
+
+////////////////////////////////////
+
+function reverseArray28(array) {
+    let temp = 0;
+    for (left = 0, right = (array.length - 1); left < right; left++, right--) {
+        temp = array[left];
+        array[left] = array[right];
+        array[right] = temp;
+    }
+    return array;
+}
+er = [9, 8, 7, 6]
+console.log(reverseArray28(er));
+console.log('---------10/02/17------------');
+
+function reverseArray29(array) {
+    let temp = 0;
+    for (i = 0; i < (array.length / 2); i++) {
+        temp = array[i];
+        array[i] = array[(array.length - 1) - i];
+        array[(array.length - 1) - i] = temp;
+    }
+    return array
+}
+ty = [6, 5, 4, 3]
+console.log(reverseArray29(ty));
+console.log('---------10/02/17------------');
+
+////////////////////////////////////
