c1cadb77f3cb8b913a1d28ccc237e6624fcc9370
Refayat Haque <refayathaque@gmail.com>
2017-10-03 15:20:00 -0400

    python fizzbuzz

--- AlgorithmPractice/RandomAlgorithms.py
+++ AlgorithmPractice/RandomAlgorithms.py
@@ -233,3 +233,16 @@ def reverselistfiveALT(list):
 a = [1, 2, 3, 4]
 print reverselistfiveALT(a)
 print('------------10/02/17------------')
+
+def fizzbuzz8():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print 'FizzBuzz'
+        elif i % 3 == 0:
+            print 'Fizz'
+        elif i % 5 == 0:
+            print 'Buzz'
+        else:
+            print i
+fizzbuzz8()
+print('------------10/03/17------------')
