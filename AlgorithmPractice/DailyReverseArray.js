c497beb581a2e0aeb7f748b3eccb57c9db2a3cdf
Refayat Haque <refayathaque@gmail.com>
2017-10-04 11:07:25 -0400

    js reverse array

--- AlgorithmPractice/DailyReverseArray.js
+++ AlgorithmPractice/DailyReverseArray.js
@@ -471,3 +471,28 @@ console.log(reverseArray30(ty));
 console.log('---------10/03/17------------');
 
 ////////////////////////////////////
+
+function reverseArray32(arr) {
+    let temp = 0
+    for (left = 0, right = arr.length - 1; left < right; left++, right--) {
+        temp = arr[left]
+        arr[left] = arr[right]
+        arr[right] = temp
+    }
+    return arr
+}
+ui = [4, 5, 6, 7]
+console.log(reverseArray32(ui));
+console.log('---------10/04/17------------');
+
+function reverseArray33(arr) {
+    let temp = 0
+    for (i = 0; i < arr.length / 2; i++) {
+        temp = arr[i]
+        arr[i] = arr[(arr.length - 1) - i]
+        arr[(arr.length - 1) - i] = temp
+    }
+    return arr
+}
+console.log(reverseArray33(ui));
+console.log('---------10/04/17------------');
