d1aa63bfed027d347abcca3c953043e9c31a014f
Refayat Haque <refayathaque@gmail.com>
2017-10-17 09:05:56 -0400

    node fizzbuzz

--- AlgorithmPractice/DailyFizzBuzz.js
+++ AlgorithmPractice/DailyFizzBuzz.js
@@ -610,3 +610,24 @@ const fizzbuzz19 = () => {
 }
 fizzbuzz19()
 console.log('--------10/16/17-------------');
+
+const fizzbuzz20 = () => {
+    let i = 1
+    while (i < 101) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log('FizzBuzz');
+        }
+        else if (i % 3 == 0) {
+            console.log('Fizz');
+        }
+        else if (i % 5 == 0) {
+            console.log('Buzz');
+        }
+        else {
+            console.log(i);
+        }
+        i++
+    }
+}
+fizzbuzz20()
+console.log('--------10/17/17-------------');
